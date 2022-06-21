504d401455fed17bc4cf39fabc7d81adef8becc8
plvhx <gandung@slackerc0de.us>
2022-06-21 11:24:13 +0700

    :)

--- compiler/compiler.h
+++ compiler/compiler.h
@@ -107,12 +107,12 @@ static void ftrace_likely_update(struct ftrace_likely_data *f, int val,
     // mechanism in assembly.
     f->data.correct++;
 #elif defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
-    __asm__ __volatile__("lock incl %0\n"
+    __asm__ __volatile__("lock; incl %0\n"
                          : "=r"(f->data.correct)
                          : "r"(f->data.correct)
                          : "memory");
 #elif defined(__x86_64__) || defined(_M_X64)
-    __asm__ __volatile__("lock incq %0\n"
+    __asm__ __volatile__("lock; incq %0\n"
                          : "=r"(f->data.correct)
                          : "r"(f->data.correct)
                          : "memory");
@@ -123,12 +123,12 @@ static void ftrace_likely_update(struct ftrace_likely_data *f, int val,
     // mechanism in assembly.
     f->data.incorrect++;
 #elif defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
-    __asm__ __volatile__("lock incl %0\n"
+    __asm__ __volatile__("lock; incl %0\n"
                          : "=r"(f->data.incorrect)
                          : "r"(f->data.incorrect)
                          : "memory");
 #elif defined(__x86_64__) || defined(_M_X64)
-    __asm__ __volatile__("lock incq %0\n"
+    __asm__ __volatile__("lock; incq %0\n"
                          : "=r"(f->data.incorrect)
                          : "r"(f->data.incorrect)
                          : "memory");
